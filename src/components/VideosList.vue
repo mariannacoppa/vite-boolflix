a45aefb19c730bbd8760db84f7222f54f6fa8dc5
mariannacoppa <densimetra@gmail.com>
2024-08-27 18:10:33 +0200

    [UPD] added video item structure

--- src/components/VideosList.vue
+++ src/components/VideosList.vue
@@ -22,8 +22,34 @@ export default {
 <template>
     <div>
         video item
+        <div class="container">
+            <div class="row-list">
+                <div class="col-20" v-for="video in store.videoList" :key="video.id">
+                    <div class="video">
+                        <img :src="video.poster_path" alt="">
+                        <h6>
+                            {{ video.title }}
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style lang="scss">
-    
+<style lang="scss" scoped>
+    .row-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    .col-20 {
+        width: calc(100% / 5);
+    }
+    .video {
+        background-color: burlywood;
+        margin: 10px;
+        img {
+            max-width: 100%;
+        }
+    }
 </style>
